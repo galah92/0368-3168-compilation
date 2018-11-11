81a3c67172d54caedd3e1b077ddb3aefe2594fad
galah92 <galaharoni92@gmail.com>
2018-11-11 10:57:53 +0200

    [EX2] fixed varDec

--- EX2/FOLDER_2_SRC/AST/AST_Node.java
+++ EX2/FOLDER_2_SRC/AST/AST_Node.java
@@ -5,7 +5,7 @@ public abstract class AST_Node
 	/* The serial number is for debug purposes */
 	/* In particular, it can help in creating  */
 	/* a graphviz dot format of the AST ...    */
-	public int SerialNumber;
+	public int SerialNumber = AST_Node_Serial_Number.getFresh();
 	
 	/* The default message for an unknown AST node */
 	public void PrintMe()
--- EX2/FOLDER_2_SRC/AST/AST_varDec.java
+++ EX2/FOLDER_2_SRC/AST/AST_varDec.java
@@ -5,16 +5,26 @@ public class AST_varDec extends AST_Node
 	public String id1;
 	public String id2;
 	public AST_EXP exp;
+	public AST_newExp newExp;
+	
+	public AST_varDec(String id1, String id2)
+	{
+		this.id1 = id1;
+		this.id2 = id2;
+	}
 	
 	public AST_varDec(String id1, String id2, AST_EXP exp)
 	{
-		SerialNumber = AST_Node_Serial_Number.getFresh();
-
-		System.out.format("varDec -> ID(%s) ID(%s) LBRACK ASSIGN exp RBRACK SEMICOLON\n", id1, id2);
-
 		this.id1 = id1;
 		this.id2 = id2;
 		this.exp = exp;
+	}
+	
+	public AST_varDec(String id1, String id2, AST_newExp newExp)
+	{
+		this.id1 = id1;
+		this.id2 = id2;
+		this.newExp = newExp;
 	}
 
 	public void PrintMe()
@@ -23,16 +33,12 @@ public class AST_varDec extends AST_Node
 
 		System.out.format("ID1(%s)\n", id1);
 		System.out.format("ID2(%s)\n", id2);
-		exp.PrintMe();
+		if (exp != null) { exp.PrintMe(); }
+		if (newExp != null) { newExp.PrintMe(); }
 
-		AST_GRAPHVIZ.getInstance().logNode(
-			SerialNumber,
-			String.format("ID1\n...->%s", id1));
-		AST_GRAPHVIZ.getInstance().logNode(
-			SerialNumber,
-			String.format("ID2\n...->%s", id2));
-		AST_GRAPHVIZ.getInstance().logEdge(
-			SerialNumber,
-			exp.SerialNumber);
+		AST_GRAPHVIZ.getInstance().logNode(SerialNumber, String.format("ID1\n...->%s", id1));
+		AST_GRAPHVIZ.getInstance().logNode(SerialNumber, String.format("ID2\n...->%s", id2));
+		if (exp != null) { AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, exp.SerialNumber); }
+		if (newExp != null) { AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, newExp.SerialNumber); }
 	}
 }
