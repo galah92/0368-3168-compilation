3b2358e8bbb8458b256b5534b0b4480b9de7a24b
galah92 <galaharoni92@gmail.com>
2018-11-13 08:20:27 +0200

    [EX2] fixed STMT_ASSIGN to assign newExp

--- EX2/FOLDER_2_SRC/AST/AST_STMT_ASSIGN.java
+++ EX2/FOLDER_2_SRC/AST/AST_STMT_ASSIGN.java
@@ -2,61 +2,37 @@ package AST;
 
 public class AST_STMT_ASSIGN extends AST_STMT
 {
-	/***************/
-	/*  var := exp */
-	/***************/
 	public AST_VAR var;
 	public AST_EXP exp;
+    public AST_newExp newExp;
 
-	/*******************/
-	/*  CONSTRUCTOR(S) */
-	/*******************/
-	public AST_STMT_ASSIGN(AST_VAR var,AST_EXP exp)
+	public AST_STMT_ASSIGN(AST_VAR var,AST_newExp newExp)
 	{
-		/******************************/
-		/* SET A UNIQUE SERIAL NUMBER */
-		/******************************/
 		SerialNumber = AST_Node_Serial_Number.getFresh();
 
-		/***************************************/
-		/* PRINT CORRESPONDING DERIVATION RULE */
-		/***************************************/
-		System.out.print("====================== stmt -> var ASSIGN exp SEMICOLON\n");
+		this.var = var;
+		this.newExp = newExp;
+	}
 
-		/*******************************/
-		/* COPY INPUT DATA NENBERS ... */
-		/*******************************/
+	public AST_STMT_ASSIGN(AST_VAR var,AST_EXP exp)
+	{
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
 		this.var = var;
 		this.exp = exp;
 	}
 
-	/*********************************************************/
-	/* The printing message for an assign statement AST node */
-	/*********************************************************/
 	public void PrintMe()
 	{
-		/********************************************/
-		/* AST NODE TYPE = AST ASSIGNMENT STATEMENT */
-		/********************************************/
 		System.out.print("AST NODE ASSIGN STMT\n");
 
-		/***********************************/
-		/* RECURSIVELY PRINT VAR + EXP ... */
-		/***********************************/
 		if (var != null) var.PrintMe();
 		if (exp != null) exp.PrintMe();
+		if (newExp != null) newExp.PrintMe();
 
-		/***************************************/
-		/* PRINT Node to AST GRAPHVIZ DOT file */
-		/***************************************/
-		AST_GRAPHVIZ.getInstance().logNode(
-			SerialNumber,
-			"ASSIGN\nleft := right\n");
-		
-		/****************************************/
-		/* PRINT Edges to AST GRAPHVIZ DOT file */
-		/****************************************/
+		AST_GRAPHVIZ.getInstance().logNode(SerialNumber, "ASSIGN\nleft := right\n");
 		AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,var.SerialNumber);
-		AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,exp.SerialNumber);
+		if (exp != null) { AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, exp.SerialNumber); }
+		if (newExp != null) { AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, newExp.SerialNumber); }
 	}
 }
