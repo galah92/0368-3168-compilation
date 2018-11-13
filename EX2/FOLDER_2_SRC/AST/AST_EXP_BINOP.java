f6a46ba436cf5cd7cb55bac5d20c0ac4956b5ad6
nitzanyogev <nitzany15@gmail.com>
2018-11-13 10:43:34 -0500

    all operators

--- EX2/FOLDER_2_SRC/AST/AST_EXP_BINOP.java
+++ EX2/FOLDER_2_SRC/AST/AST_EXP_BINOP.java
@@ -5,7 +5,7 @@ public class AST_EXP_BINOP extends AST_EXP
 	int OP;
 	public AST_EXP left;
 	public AST_EXP right;
-	
+
 	public AST_EXP_BINOP(AST_EXP left,AST_EXP right,int OP)
 	{
 		/* SET A UNIQUE SERIAL NUMBER */
@@ -17,20 +17,42 @@ public class AST_EXP_BINOP extends AST_EXP
 		this.right = right;
 		this.OP = OP;
 	}
-	
+
 	public void PrintMe()
 	{
 		String sOP="";
-		
-		if (OP == 0) { sOP = "+"; }
-		if (OP == 1) { sOP = "-"; }
-		
+		switch(OP){
+			case 0:
+				sOP = "+";
+				break;
+			case 1 :
+				sOP = "-";
+				break;
+			case 2:
+				sOP = "*";
+				break;
+			case 3:
+				sOP = "/";
+				break;
+			case 4:
+				sOP = "<";
+				break;
+			case 5:
+				sOP = ">";
+				break;
+			case 6:
+				sOP = "=";
+				break;
+			default:
+				sOP = "";
+		}
+	
 		System.out.println("AST NODE BINOP EXP");
 		if (left != null) left.PrintMe();
 		if (right != null) right.PrintMe();
-		
+
 		AST_GRAPHVIZ.getInstance().logNode(SerialNumber, String.format("BINOP(%s)", sOP));
-		
+
 		if (left  != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, left.SerialNumber);
 		if (right != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, right.SerialNumber);
 	}
