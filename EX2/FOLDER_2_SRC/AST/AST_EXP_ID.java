f0c066be34c8fa45fa8fb9b25117e4ce88086912
nitzanyogev <nitzany15@gmail.com>
2018-11-15 11:41:44 -0500

    exp should be working - need to recheck

---
+++ EX2/FOLDER_2_SRC/AST/AST_EXP_ID.java
@@ -0,0 +1,80 @@
+package AST;
+
+public class AST_EXP_ID extends AST_EXP
+{
+	public String id;
+	public AST_expList expList;
+	public AST_VAR var;
+
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_EXP_ID(String id, AST_expList expList)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.print("====================== exp -> ID LPAREN expList RPAREN\n");
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.id = id;
+		this.expList = expList;
+	}
+
+public AST_EXP_ID(AST_VAR var, String id, AST_expList expList)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.print("====================== exp -> var DOT ID LPAREN expList RPAREN\n");
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.var = var;
+		this.id = id;
+		this.expList = expList;
+	}
+	
+	
+	public void PrintMe()
+	{
+		/************************************/
+		/* AST NODE TYPE = EXP ID LPAREN expList RPAREN AST NODE */
+		/************************************/
+		if(var == null) System.out.print(" ID LPAREN expList RPAREN\n");
+		else System.out.print(" ID VAR DOT LPAREN expList RPAREN\n");
+		/*****************************/
+		/* RECURSIVELY PRINT exp ... */
+		/*****************************/
+		if (var != null) var.PrintMe();
+		if (expList != null) expList.PrintMe();
+		
+		/*********************************/
+		/* Print to AST GRAPHIZ DOT file */
+		/*********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			 String.format("ID1->%s", id));
+
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		//AST_GRAPHVIZ.getInstance().logNode(SerialNumber, "Lparen exp Rparen");
+		if (var != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,var.SerialNumber);
+		AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,expList.SerialNumber);
+			
+	}
+}
