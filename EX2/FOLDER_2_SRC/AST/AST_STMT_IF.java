5ea22a48096ff34a334f93a9c10bae300235055f
Eyal Gomel <eyalgomel12@gmail.com>
2018-11-16 15:48:39 +0200

    AST WHILE&IF classed implemented

--- EX2/FOLDER_2_SRC/AST/AST_STMT_IF.java
+++ EX2/FOLDER_2_SRC/AST/AST_STMT_IF.java
@@ -10,7 +10,17 @@ public class AST_STMT_IF extends AST_STMT
 	/*******************/
 	public AST_STMT_IF(AST_EXP cond,AST_STMT_LIST body)
 	{
+		SerialNumber = AST_Node_Serial_Number.getFresh();
 		this.cond = cond;
 		this.body = body;
 	}
+
+	public void PrintMe(){
+		System.out.format("AST_STMT_IF\n");
+		if (cond != null) cond.PrintMe();
+		if (body != null) body.PrintMe();
+		AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, cond.SerialNumber);
+		AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, body.SerialNumber);
+	}
+
 }
--- EX2/FOLDER_2_SRC/AST/AST_STMT_WHILE.java
+++ EX2/FOLDER_2_SRC/AST/AST_STMT_WHILE.java
@@ -9,8 +9,18 @@ public class AST_STMT_WHILE extends AST_STMT
 	/*  CONSTRUCTOR(S) */
 	/*******************/
 	public AST_STMT_WHILE(AST_EXP cond,AST_STMT_LIST body)
-	{
+{
+		SerialNumber = AST_Node_Serial_Number.getFresh();
 		this.cond = cond;
 		this.body = body;
 	}
+
+	public void PrintMe(){
+		System.out.format("AST_STMT_WHILE\n");
+		if (cond != null) cond.PrintMe();
+		if (body != null) body.PrintMe();
+		AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, cond.SerialNumber);
+		AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, body.SerialNumber);
+	}
+
 }
