6b1724cf8c9a5c92a9f67ec6b68ed59acc148508
galah92 <galaharoni92@gmail.com>
2018-11-18 12:16:50 +0200

    [EX2] now writer errors to output file

--- EX2/FOLDER_2_SRC/Main.java
+++ EX2/FOLDER_2_SRC/Main.java
@@ -1,4 +1,3 @@
-   
 import java.io.*;
 import java.io.PrintWriter;
 import java_cup.runtime.Symbol;
@@ -10,14 +9,14 @@ public class Main
 	{
 		try
 		{
-			Parser p = new Parser(new Lexer(new FileReader(argv[0])));
-			AST_STMT_LIST AST = (AST_STMT_LIST) p.parse().value;
+			PrintWriter pwriter = new PrintWriter(argv[1]);
+			Parser p = new Parser(new Lexer(new FileReader(argv[0])), pwriter);
+			AST_STMT_LIST AST = (AST_STMT_LIST)p.parse().value;
 			AST.PrintMe();
-			PrintWriter file_writer = new PrintWriter(argv[1]);
-            file_writer.print("OK");
-			file_writer.close();
+            pwriter.print("OK");
+			pwriter.close();
 			AST_GRAPHVIZ.getInstance().finalizeFile();
-    		}
+    	}
 		catch (Exception e)
 		{
 			e.printStackTrace();
