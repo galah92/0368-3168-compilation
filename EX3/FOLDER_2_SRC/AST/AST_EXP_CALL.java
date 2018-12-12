1f73d5ca00624e7deaabf4a427d78e16e3d819d1
nitzanyogev <nitzany15@gmail.com>
2018-12-12 06:52:44 -0500

    AST_EXP_CALL - SEMANTME - REVIEW NEEDED

--- EX3/FOLDER_2_SRC/AST/AST_EXP_CALL.java
+++ EX3/FOLDER_2_SRC/AST/AST_EXP_CALL.java
@@ -36,17 +36,37 @@ public class AST_EXP_CALL extends AST_EXP
 		/* RECURSIVELY PRINT params + body ... */
 		/***************************************/
 		if (params != null) params.PrintMe();
-		
+
 		/***************************************/
 		/* PRINT Node to AST GRAPHVIZ DOT file */
 		/***************************************/
 		AST_GRAPHVIZ.getInstance().logNode(
 			SerialNumber,
 			String.format("CALL(%s)\nWITH",funcName));
-		
+
 		/****************************************/
 		/* PRINT Edges to AST GRAPHVIZ DOT file */
 		/****************************************/
-		AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,params.SerialNumber);		
+		AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,params.SerialNumber);
+	}
+
+	public TYPE SemantMe(){
+		TYPE t;
+		TYPE type_func = SYMBOL_TABLE.getInstance().find(this.funcName);
+		if (ret_type == null){
+			System.out.format(">> ERROR [%d:%d] non existing function %s\n",99,99,this.funcName;
+		}
+
+		for(AST_EXP_LIST el = paramas, TYPE_LIST tl = type_func.params; el != null && tl != null ; el = el.tail, tl = tl.tail){
+			t = SYMBOL_TABLE.getInstance().find(el.head.name);
+			if(tl != t)
+				System.out.format(">> ERROR [%d:%d] arguments types dont fit in func %s\n",99,99,this.funcName;
+
+		}
+		if (el != tl) /* one of them is not null and therefore there is an error */
+			System.out.format(">> ERROR [%d:%d] number of arguments doesnt fit function %s\n",99,99,this.funcName;
+
+
+		return ret_type.returnType;
 	}
 }
