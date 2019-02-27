34cd5da7798a686a0aab08a3e09ecc5f08aee7a6
galah92 <galaharoni92@gmail.com>
2019-02-27 15:41:32 +0200

    [EX4] MIPSGen WIP

--- EX4/FOLDER_2_SRC/MIPSGen.java
+++ EX4/FOLDER_2_SRC/MIPSGen.java
@@ -10,7 +10,7 @@ public class MIPSGen
 
     public static void toFile(String outFile) throws Exception
     {
-        writer.print("\tli $v0,10\n");
+        writer.print("\tli $v0, 10\n");
         writer.print("\tsyscall\n");
         writer.close();
 
@@ -22,6 +22,8 @@ public class MIPSGen
         fileWriter.append(stringWriter.toString());
         fileWriter.close();
     }
+
+    public static final int WORD = 4;
 
     public static final String zero = "zero";  // the value 0, not changeable
 
@@ -39,45 +41,25 @@ public class MIPSGen
     public static final String fp = "$fp";  // frame pointer (bottom of current stack frame)
     public static final String ra = "$ra";  // return address of most recent caller
 
-    public static void add(TempReg dst, TempReg reg1, TempReg reg2)
-    {
-        writer.printf("\tadd Temp_%d, Temp_%d, Temp_%d\n", dst.id, reg1.id, reg2.id);
-    }
+    public static void add(TempReg dst, TempReg reg1, TempReg reg2) { writer.printf("\tadd Temp_%d, Temp_%d, Temp_%d\n", dst.id, reg1.id, reg2.id); }
 
-    public static void addi(TempReg dst, TempReg reg1, int imm)
-    {
-        writer.printf("\tadd Temp_%d, Temp_%d, Temp_%d\n", dst.id, reg1.id, imm);
-    }
+    public static void addi(TempReg rd, TempReg rs, int imm) { writer.printf("\taddi Temp_%d, Temp_%d, Temp_%d\n", rd.id, rs.id, imm); }
 
-    public static void mul(TempReg dst, TempReg reg1, TempReg reg2)
-    {
-        writer.printf("\tmul Temp_%d, Temp_%d, Temp_%d\n", dst.id, reg1.id, reg2.id);
-    }
+    public static void mul(TempReg dst, TempReg reg1, TempReg reg2) { writer.printf("\tmul Temp_%d, Temp_%d, Temp_%d\n", dst.id, reg1.id, reg2.id); }
 
-    public static void move(TempReg dst, TempReg reg1)
-    {
-        writer.printf("\tmove Temp_%d, Temp_%d\n", dst.id, reg1.id);
-    }
+    public static void move(TempReg dst, TempReg reg1) { writer.printf("\tmove Temp_%d, Temp_%d\n", dst.id, reg1.id); }
+    public static void move(String dst, TempReg reg1) { writer.printf("\tmove %s, Temp_%d\n", dst, reg1.id); }
 
-    public static void move(String dst, TempReg reg1)
-    {
-        writer.printf("\tmove %s, Temp_%d\n", dst, reg1.id);
-    }
+    public static void li(TempReg d, int imm) { writer.printf("\tli Temp_%d, %d\n", d.id, imm); }
+    public static void li(String d, int imm) { writer.printf("\tli %s, %d\n", d, imm); }
 
-    public static void li(TempReg d, int addr)
-    {
-        writer.printf("\tli Temp_%d, %d\n", d.id, addr);
-    }
+    public static void lw(String rd, String addr) { writer.printf("\tlw %s, %s\n", rd, addr); }
 
-    public static void li(String d, int addr)
-    {
-        writer.printf("\tli %s, %d\n", d, addr);
-    }
+    public static void sw(String rs, String addr) { writer.printf("\tsw %s, %s\n", rs, addr); }
 
-    public static void syscall()
-    {
-        writer.printf("\tsyscall\n");
-    }
+    public static void jr(String rs) { writer.printf("\tjr %s\n", rs); }
+
+    public static void syscall() { writer.printf("\tsyscall\n"); }
 
     public static void print_string(TempReg t)
     {
@@ -109,40 +91,19 @@ public class MIPSGen
         writer.printf("%s:\n", inlabel);
     }	
 
-    public static void jump(String inlabel)
-    {
-        writer.printf("\tj %s\n", inlabel);
-    }	
+    public static void jump(String inlabel) { writer.printf("\tj %s\n", inlabel); }
 
-    public static void blt(TempReg reg1, TempReg reg2, String label)
-    {
-        writer.printf("\tblt Temp_%d, Temp_%d, %s\n", reg1.id, reg2.id, label);
-    }
+    public static void blt(TempReg reg1, TempReg reg2, String label) { writer.printf("\tblt Temp_%d, Temp_%d, %s\n", reg1.id, reg2.id, label); }
 
-    public static void bge(TempReg reg1, TempReg reg2, String label)
-    {
-        writer.printf("\tbge Temp_%d, Temp_%d, %s\n", reg1.id, reg2.id, label);
-    }
+    public static void bge(TempReg reg1, TempReg reg2, String label) { writer.printf("\tbge Temp_%d, Temp_%d, %s\n", reg1.id, reg2.id, label); }
 
-    public static void bne(TempReg reg1,TempReg reg2,String label)
-    {
-        writer.printf("\tbne Temp_%d, Temp_%d, %s\n", reg1.id, reg2.id, label);
-    }
+    public static void bne(TempReg reg1,TempReg reg2,String label) { writer.printf("\tbne Temp_%d, Temp_%d, %s\n", reg1.id, reg2.id, label); }
 
-    public static void beq(TempReg reg1,TempReg reg2,String label)
-    {
-        writer.printf("\tbeq Temp_%d, Temp_%d, %s\n", reg1.id, reg2.id, label);
-    }
+    public static void beq(TempReg reg1,TempReg reg2,String label) { writer.printf("\tbeq Temp_%d, Temp_%d, %s\n", reg1.id, reg2.id, label); }
 
-    public static void beqz(TempReg reg1, String label)
-    {
-        writer.printf("\tbeq Temp_%d, $zero,%s\n", reg1.id, label);
-    }
+    public static void beqz(TempReg reg1, String label) { writer.printf("\tbeq Temp_%d, $zero, %s\n", reg1.id, label); }
 
-    public static void la(TempReg reg, String label)
-    {
-        writer.printf("\tla Temp_%d, %s\n", reg.id, label);
-    }
+    public static void la(TempReg reg, String label) { writer.printf("\tla Temp_%d, %s\n", reg.id, label); }
 
     public static void func_prologue(int numLocals)
     {
@@ -153,13 +114,13 @@ public class MIPSGen
         // set fp = sp
         writer.printf("\taddi $fp, $sp, 0\n");
 	    // allocate stack frame
-        writer.printf("\taddi $sp, $sp, %d\n", -4 * numLocals);
+        writer.printf("\taddi $sp, $sp, %d\n", -WORD * numLocals);
     }
 
     public static void func_epilogue(int numLocals)
     {
         // deallocate stack frame
-        writer.printf("\taddi $sp, $sp, %d\n", 4 * numLocals);
+        writer.printf("\taddi $sp, $sp, %d\n", WORD * numLocals);
         // restore fp & ra
         writer.printf("\tlw	$ra, 4($fp)\n");
         writer.printf("\tlw	$fp, ($fp)\n");
